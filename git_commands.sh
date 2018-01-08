884ce7289c790522b534759d4ebf223b8b55503e
Sanju <san43ju@gmail.com>
2018-01-08 18:36:36 -0500

    git commands

---
+++ git_commands.sh
@@ -0,0 +1,13 @@
+
+echo "# commands" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/san43ju/commands.git
+git push -u origin master
+
+
+
+or push an existing repository from the command line
+git remote add origin https://github.com/san43ju/commands.git
+git push -u origin master
